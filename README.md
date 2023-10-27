5e7b632bbc775ca49bfd61abdf2759ddfaebdd55
Badlakes <89754605+Badlakes@users.noreply.github.com>
2023-10-27 06:33:18 -0300

    Atualização com outras interfaces e correção de pesquisa
    Adicionados novos menus e corrigidos erros de requisição

--- README.md
+++
@@ -1,3 +0,0 @@
-# GadgetTcg.io
-Projeto pessoal de site de interação e reação com API de busca de cards YGOPRODECK
-Link funcional em: https://badlakes.github.io/GadgetTcg.io/
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//---------------------------- CARREGAR CARTAS
+
 let allCards = [];
 
 function loadAllCards() {
@@ -25,24 +27,21 @@ function loadAllCards() {
         });
 }
 
+//---------------------------- PROCURAR CARTAS
+
+const cardTable = document.createElement('table');
+cardTable.className = 'table table-striped table-bordered';
+cardTable.style.width = '100%';
+cardTable.id = 'cardTable'; // Atribua um ID à tabela
+
 function searchCards() {
     const searchInput = document.getElementById('searchInput').value.toLowerCase();
     const filteredCards = allCards.filter(card => {
         return card.name.toLowerCase().includes(searchInput) || card.desc.toLowerCase().includes(searchInput);
     });
-
-
     const cardList = document.getElementById('cardList');
-    cardList.innerHTML = ''; // Limpar a lista antes de exibir novos resultados
+    cardTable.innerHTML = '';
 
-    // Exibir cartas filtradas
-    // Criar uma tabela para exibir as cartas em grade
-    const cardTable = document.createElement('table');
-    cardTable.id = 'cardTable';
-    cardTable.className = 'table table-striped table-bordered';
-    cardTable.style.width = '100%';
-
-    // Criar uma linha para cada carta encontrada
     filteredCards.forEach(card => {
         const cardRow = document.createElement('tr');
 
@@ -58,7 +57,148 @@ function searchCards() {
         // Adicionar a linha da carta à tabela
         cardTable.appendChild(cardRow);
     });
-
     // Adicionar a tabela ao documento
     document.body.appendChild(cardTable);
+    
+}
+
+/*
+//---------------------------- IMAGEM CÂMERA
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const captureButton = document.getElementById('captureBtn');
+
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+        video.srcObject = stream;
+    })
+    .catch(function (error) {
+        console.error("Erro ao acessar a câmera: ", error);
+    });
+
+    captureButton.addEventListener('click', function () {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    const imageDataUrl = canvas.toDataURL('/image/png');
+    window.open(imageDataUrl);
+});
+ */
+
+//---------------------------- PESQUISAR CARTA ESPECÍFICA
+
+var infoCarta = '';
+function loadDarkMagician() {
+    let searchURL = `https://db.ygoprodeck.com/api/v7/cardinfo.php?name=mago%20negro&language=pt`;
+  
+    fetch(searchURL)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro ao carregar cartas: ' + response.statusText);
+        }
+        return response.json();
+      })
+      .then(data => {
+
+      const darkMagicianInfoDiv = document.getElementById('dark-magician-info');
+  
+      const darkMagicianImage = document.createElement('img');
+      darkMagicianImage.src = data.data[0].card_images[0].image_url;
+
+      const darkMagicianNameParagraph = document.createElement('p');
+      infoCarta +=  darkMagicianNameParagraph.textContent = data.data[0].name;
+
+      const darkMagicianTypeParagraph = document.createElement('p');
+      infoCarta += darkMagicianTypeParagraph.textContent = data.data[0].type;
+
+      const darkMagicianAttributeParagraph = document.createElement('p');
+      infoCarta += darkMagicianAttributeParagraph.textContent = data.data[0].attribute;
+
+      const darkMagicianLevelParagraph = document.createElement('p');
+      infoCarta += darkMagicianLevelParagraph.textContent = `Nível: ${data.data[0].level}`;
+
+      const darkMagicianAttackParagraph = document.createElement('p');
+      infoCarta += darkMagicianAttackParagraph.textContent = `Ataque: ${data.data[0].atk}`;
+
+      const darkMagicianDefenseParagraph = document.createElement('p');
+      infoCarta += darkMagicianDefenseParagraph.textContent = `Defesa: ${data.data[0].def}`;
+
+      const darkMagicianEffectParagraph = document.createElement('p');
+      darkMagicianEffectParagraph.textContent = data.data[0].desc;
+
+      infoCarta += darkMagicianEffectParagraph.textContent = darkMagicianEffectParagraph.textContent.replace(/\n/g, '<br />');
+
+      darkMagicianInfoDiv.appendChild(darkMagicianImage);
+      darkMagicianInfoDiv.appendChild(darkMagicianNameParagraph);
+      darkMagicianInfoDiv.appendChild(darkMagicianTypeParagraph);
+      darkMagicianInfoDiv.appendChild(darkMagicianAttributeParagraph);
+      darkMagicianInfoDiv.appendChild(darkMagicianLevelParagraph);
+      darkMagicianInfoDiv.appendChild(darkMagicianAttackParagraph);
+      darkMagicianInfoDiv.appendChild(darkMagicianDefenseParagraph);
+      darkMagicianInfoDiv.appendChild(darkMagicianEffectParagraph);
+    })
+    .catch(error => {
+        console.log("Deu ruim chefe")
+    })
+}
+
+//---------------------------- TTS CARTA ESPECÍFICA
+
+var voiceList = document.querySelector('#voiceList');
+var btnSpeak = document.querySelector('#btnSpeak');
+
+var tts = window.speechSynthesis;
+var voices = [];
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    GetVoices();
+});
+
+if(speechSynthesis !== undefined){
+    speechSynthesis.onvoiceschanged = GetVoices;
+}
+if (btnSpeak !=null){
+btnSpeak.addEventListener('click', ()=>{
+    var toSpeak = new SpeechSynthesisUtterance(infoCarta);
+    var selectedVoiceName = voiceList.options[voiceList.selectedIndex].getAttribute('data-name');
+    voices.forEach((voice)=>{
+        if(voice.name === selectedVoiceName){
+            toSpeak.voice = voice;
+        }
+    });
+
+    tts.speak(toSpeak);
+})
+}
+
+
+function GetVoices(){
+    const speechSynthesis = window.speechSynthesis;
+
+    const speak = (text) => {
+    const utterance = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(utterance);
+    };
+
+    const buttonGetVoice = document.querySelector('#btnSpeak');
+
+    if (buttonGetVoice != null){
+        buttonGetVoice.addEventListener('click', () => {
+        const text = document.querySelector('p').value;
+        speak(text);
+        });
+    }
+
+
+    voices = tts.getVoices();
+    if (voiceList != null){
+        voiceList.innerHTML = '';
+        voices.forEach((voice)=>{
+            var listItem = document.createElement('option');
+            listItem.textContent = voice.name;
+            listItem.setAttribute('data-lang', voice.lang)
+            listItem.setAttribute('data-name', voice.name)
+            voiceList.appendChild(listItem);
+        });
+
+        voiceList.selectedIndex = 0;
+    }
 }
